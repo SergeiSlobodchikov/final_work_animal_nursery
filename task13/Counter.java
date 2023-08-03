d22787de9360e0b20aadb5a836b3f0293a543a23
Sergei Slobodchikov <95425551+SergeiSlobodchikov@users.noreply.github.com>
2023-08-03 10:45:14 +0300

    Итоговая работа

---
+++ task13/Counter.java
@@ -0,0 +1,24 @@
+package task13;
+
+public class Counter implements AutoCloseable {
+    private int count;
+
+    public Counter() {
+        count = 0;
+    }
+
+    public void add() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void close() {
+        if (count > 0) {
+            throw new RuntimeException("Ошибка: Объект типа Счетчик был использован вне ресурсного try.");
+        }
+    }
+}
